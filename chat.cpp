d82d47348de304ffca71cadb971baf0b035717c3
IssaParty <101088744+IssaParty@users.noreply.github.com>
2022-10-24 14:10:53 -0600

    Create chat.cpp

---
+++ chat.cpp
@@ -0,0 +1,23 @@
+/*******************************************
+* Group Name  : FLCL
+
+* Member1 Name: Issa Ahmed
+* Member1 SIS ID: 832161534
+* Member1 Login ID: aissaam
+
+* Member2 Name: Carlos Flores
+* Member2 SIS ID: 832394284
+* Member2 Login ID: carlosf
+
+* Member3 Name: Yosief Gebremedhin
+* Member3 SIS ID: 830031364
+* Member3 Login ID: ygebreme
+********************************************/
+
+
+
+
+int main(int argc, char* argv[]){
+
+  return 0;
+}
