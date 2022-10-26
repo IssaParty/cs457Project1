dcf383d67e7c19371554c526afdbdc5829cedeb9
Carlos Flores <carlosf@colostate.edu>
2022-10-26 11:10:03 -0600

    retrieved ip info using gethostname and gethostbyname

--- chat.cpp
+++ chat.cpp
@@ -15,6 +15,9 @@
 ********************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -65,7 +68,7 @@ printf("\n");
 6.  Send the message to the client. 
 7.  GOTO step 3. 
 */
-
+return 0;
 }
 
 int clientSide(){
@@ -78,9 +81,52 @@ int clientSide(){
 5.  Receive message and print to screen. 
 6.  GOTO step 2. 
 */
+return 0;
+}
+void checkHostName(int hostname){
+if(hostname == -1){
+  perror("gethostname");
+  exit(1);
+}
+}
 
+void checkHostEntry(struct hostent * hostentry){
+  if(hostentry == NULL){
+    perror("gethostbyname");
+    exit(1);
+  }
+}
+
+void checkIPbuffer(char *IPbuffer){
+  if(NULL == IPbuffer){
+    perror("inet_ntoa");
+    exit(1);
+  }
 }
 
 int main(int argc, char* argv[]){
+  //serverSide() we need to use this function to connect the host with the ip and port
+    char hostbuffer[256];
+    char *IPbuffer;
+    struct hostent *host_entry;
+    int hostname;
+  
+    // To retrieve hostname
+   hostname = gethostname(hostbuffer, sizeof(hostbuffer));
+   checkHostName(hostname);
+  
+    // To retrieve host information
+    host_entry = gethostbyname(hostbuffer);
+    checkHostEntry(host_entry);
+  
+    // To convert an Internet network
+    // address into ASCII string
+    IPbuffer = inet_ntoa(*((struct in_addr*)
+                           host_entry->h_addr_list[0]));
+    checkIPbuffer(IPbuffer);
+
+    printf("Hostname: %s\n", hostbuffer);
+    printf("Host IP: %s", IPbuffer);
+    printf("\n");
   return 0;
 }
